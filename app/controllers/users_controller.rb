dcf4a124e98af07ab38208e9d1dae3aaee27d7c3
nishina <n.toshi.1.014@gmail.com>
2017-01-03 21:58:17 +0900

    :sparkles: Add showing friends info function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.getUser("1")
+    @friends = []
+    @user.friends.each do | friend |
+        friend = User.getUser(friend.to_s)
+        @friends << friend
+    end
+
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user } # jsonを指定した場合、jsonフォーマットで返す
