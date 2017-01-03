52d909f1cede8c2a8b187019fe96d49da7a387b6
nishina <n.toshi.1.014@gmail.com>
2017-01-03 22:06:27 +0900

    :sparkles: Add validating id function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,24 @@
 class UsersController < ApplicationController
   def show
-    @user = User.getUser(params[:id])
-    @friends = []
-    @user.friends.each do | friend |
-        friend = User.getUser(friend.to_s)
-        @friends << friend
-    end
+    if params[:id].present?
+      if params[:id].to_i > 5 || params[:id].to_i < 1 then
+        flash.now[:alert] = 'IDは1から5の間で入力してください'
+        render 'index' and return
+      end
+      @user = User.getUser(params[:id])
+      @friends = []
+      @user.friends.each do | friend |
+          friend = User.getUser(friend.to_s)
+          @friends << friend
+      end
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user } # jsonを指定した場合、jsonフォーマットで返す
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @user } # jsonを指定した場合、jsonフォーマットで返す
+      end
+    else
+      flash.now[:alert] = 'IDを入力してください'
+      render 'index' and return
     end
   end
 
